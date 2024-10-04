c18530b2426432002e30abe1665373a1f31a4df7
Vital <Vital@DESKTOP-K2UT91P>
2024-10-04 10:59:16 +0300

    # Conf update

--- RoadMatereal/Program.cs
+++ RoadMatereal/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<RoadMaterialContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")),
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionNote")),
     ServiceLifetime.Scoped);
 
 builder.Services.AddScoped<IMaterialService, MaterialService>();
